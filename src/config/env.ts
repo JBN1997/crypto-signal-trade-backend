96e9920315639c6efb3d5f257c4097e5a0707761
Júlio Bengtson Neves <bengtson.neves@gmail.com>
2023-01-08 10:47:47 -0300

    chore: add environment variables ops

---
+++ src/config/env.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getEnvVariable = (name: string, fallback: string = ''): string => {
+   const envVariable = process.env[name];
+   const fallbackProvided = fallback.length;
+
+   if (!envVariable && !fallbackProvided) {
+      throw new Error(`Environment variable ${name} has not been set.`);
+   }
+
+   return envVariable || fallback;
+};
+
+const CONFIG = {
+   SERVER: {
+      PORT: getEnvVariable('PORT', '3001'),
+      ENV: getEnvVariable('NODE_ENV', 'development')
+   }
+};
+
+export default CONFIG;
